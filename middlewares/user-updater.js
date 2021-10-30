ecd46dd7228d5cc2e8bfae8c7c12f885a404754d
Paul Nodet <paul.nodet@gmail.com>
2021-10-31 00:58:35 +0200

    Fix user rating updater. Closes #10

--- middlewares/user-updater.js
+++ middlewares/user-updater.js
@@ -13,65 +13,66 @@ const userUpdater = async function (request, _response, next) {
 
 	const today = Date.now();
 	const ONE_WEEK = 1000 * 7 * 24 * 60 * 60;
-	if (today > user.lastUpdatedAt + ONE_WEEK) {
+	const lastUpdate = new Date(user.lastUpdatedAt);
+	const lastUpdateInMilliseconds = lastUpdate.getTime();
+
+	if (today > lastUpdateInMilliseconds + ONE_WEEK) {
 		const lichessUser = await getLichessData(request.session.token);
-		const updateBlock = {
-			id: lichessUser.id,
-			username: lichessUser.username,
-			url: lichessUser.url,
-			lastUpdatedAt: Date.now(),
-		};
+		user.id = lichessUser.id;
+		user.username = lichessUser.username;
+		user.url = lichessUser.url;
+		user.lastUpdatedAt = today;
 
 		if (lichessUser.perfs.ultraBullet) {
-			updateBlock.perfs.ultraBullet = {
+			user.perfs.ultraBullet = {
 				games: lichessUser.perfs.ultraBullet.games,
 				rating: lichessUser.perfs.ultraBullet.rating,
 			};
 		}
 
 		if (lichessUser.perfs.bullet) {
-			updateBlock.perfs.bullet = {
+			user.perfs.bullet = {
 				games: lichessUser.perfs.bullet.games,
 				rating: lichessUser.perfs.bullet.rating,
 			};
 		}
 
 		if (lichessUser.perfs.blitz) {
-			updateBlock.perfs.blitz = {
+			user.perfs.blitz = {
 				games: lichessUser.perfs.blitz.games,
 				rating: lichessUser.perfs.blitz.rating,
 			};
 		}
 
 		if (lichessUser.perfs.rapid) {
-			updateBlock.perfs.rapid = {
+			user.perfs.rapid = {
 				games: lichessUser.perfs.rapid.games,
 				rating: lichessUser.perfs.rapid.rating,
 			};
 		}
 
 		if (lichessUser.perfs.classical) {
-			updateBlock.perfs.classical = {
+			user.perfs.classical = {
 				games: lichessUser.perfs.classical.games,
 				rating: lichessUser.perfs.classical.rating,
 			};
 		}
 
 		if (lichessUser.perfs.correspondence) {
-			updateBlock.perfs.correspondence = {
+			user.perfs.correspondence = {
 				games: lichessUser.perfs.correspondence.games,
 				rating: lichessUser.perfs.correspondence.rating,
 			};
 		}
 
 		if (lichessUser.perfs.puzzle) {
-			updateBlock.perfs.puzzle = {
+			user.perfs.puzzle = {
 				games: lichessUser.perfs.puzzle.games,
 				rating: lichessUser.perfs.puzzle.rating,
 			};
 		}
 
-		User.updateOne({id}, {$set: updateBlock}, error => {
+		user.save(error => {
 			if (error) return next(error);
 			return next();
 		});
